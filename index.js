f4815275dcd490d675282ce40f5706c065614af2
Dhrubneil <dhrubneil.56@gmail.com>
2023-11-04 20:54:24 +0600

    having issues yet

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('medicines.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(server);
